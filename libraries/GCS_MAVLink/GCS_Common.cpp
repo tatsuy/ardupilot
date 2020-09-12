31f7a5819e00e4e619ea89a4a0eb444def1824ea
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2020-09-12 09:35:48 +0900

    GCS_MAVLink: add MAV_TEXT_OPT param

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -1828,7 +1828,7 @@ void GCS_MAVLINK::send_ahrs()
 /*
     send a statustext text string to specific MAVLink bitmask
 */
-void GCS::send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, uint8_t dest_bitmask)
+void GCS::send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, uint8_t dest_bitmask, MESSAGE_OPTION opt_bitmask)
 {
     char first_piece_of_text[MAVLINK_MSG_STATUSTEXT_FIELD_TEXT_LEN+1]{};
 
@@ -1838,6 +1838,12 @@ void GCS::send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, u
         // protect the "text" member with _statustext_sem
         hal.util->vsnprintf(statustext_printf_buffer, sizeof(statustext_printf_buffer), fmt, arg_list);
         memcpy(first_piece_of_text, statustext_printf_buffer, ARRAY_SIZE(first_piece_of_text)-1);
+
+        // not send if severity level lower than MAV_TEXT_SEV
+        const AP_Vehicle *vehicle = AP::vehicle();
+        if ((uint16_t)opt_bitmask && !((uint16_t)opt_bitmask & constrain_int16(vehicle->_mav_text_option, 0, INT16_MAX))) {
+            break;
+        }
 
         // filter destination ports to only allow active ports.
         statustext_t statustext{};
--- libraries/GCS_MAVLink/GCS_Dummy.h
+++ libraries/GCS_MAVLink/GCS_Dummy.h
@@ -87,7 +87,7 @@ private:
         return (GCS_MAVLINK_Dummy *)_chan[ofs];
     };
 
-    void send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, uint8_t dest_bitmask) override {
+    void send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list, uint8_t dest_bitmask, MESSAGE_OPTION opt_bitmask =  MESSAGE_OPTION::General) override {
         hal.console->printf("TOGCS: ");
         hal.console->vprintf(fmt, arg_list);
         hal.console->printf("\n");

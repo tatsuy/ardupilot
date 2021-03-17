dd81dee48cf25c60ddf5944f2d702bae8b67f26b
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2021-03-17 17:21:43 +0900

    Rover: let EKF variance message show every 30 seconds

--- Rover/ekf_check.cpp
+++ Rover/ekf_check.cpp
@@ -55,11 +55,6 @@ void Rover::ekf_check()
 
                 AP::logger().Write_Error(LogErrorSubsystem::EKFCHECK,
                                          LogErrorCode::EKFCHECK_BAD_VARIANCE);
-                // send message to gcs
-                if ((AP_HAL::millis() - ekf_check_state.last_warn_time) > EKF_CHECK_WARNING_TIME) {
-                    gcs().send_text(MAV_SEVERITY_CRITICAL,"EKF variance");
-                    ekf_check_state.last_warn_time = AP_HAL::millis();
-                }
                 failsafe_ekf_event();
             }
         }
@@ -76,6 +71,14 @@ void Rover::ekf_check()
                 // clear failsafe
                 failsafe_ekf_off_event();
             }
+        }
+    }
+
+    if (ekf_check_state.bad_variance) {
+        // send message to gcs
+        if ((AP_HAL::millis() - ekf_check_state.last_warn_time) > EKF_CHECK_WARNING_TIME) {
+            gcs().send_text(MAV_SEVERITY_CRITICAL, "EKF variance");
+            ekf_check_state.last_warn_time = AP_HAL::millis();
         }
     }
 
